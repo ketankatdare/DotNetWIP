4e741206a3030d9f379e02e2ab6c02223131c87d
Ketan Katdare <intech.ketan@gmail.com>
2025-04-13 17:28:24 +0530

    Adds coding excercises

---
+++ DSA/CodingExercises/01_ReverseWordInSentence.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingExercises
+{
+    public class _01_ReverseWordInSentence
+    {
+        /// <summary>
+        /// The function should not use any built-in functions to reverse the string.
+        /// </summary>
+        /// <param name="sentence"></param>
+        /// <returns></returns>
+        public static string ReverseWords(string sentence)
+        {
+            if (string.IsNullOrEmpty(sentence))
+            {
+                return sentence;
+            }
+
+            Stack<string> stack = new Stack<string>();
+            StringBuilder word = new StringBuilder();
+
+            foreach (char c in sentence)
+            {
+                if (c == ' ')
+                {
+                    if (word.Length > 0)
+                    {
+                        stack.Push(word.ToString());
+                        word.Clear();
+                    }
+                }
+                else
+                {
+                    word.Append(c); 
+                }
+            }
+
+            if (word.Length > 0)
+            {
+                stack.Push(word.ToString());
+                word.Clear();
+            }
+
+            return string.Join(" ", stack);
+        }
+    }
+}
---
+++ DSA/CodingExercises/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CodingExercises;
+
+while (true)
+{
+    Console.WriteLine("Enter the excercise no you want to execute: (Enter 'Q' to exit)");
+
+    string? input = Console.ReadLine();
+    int number;
+
+    if (string.IsNullOrEmpty(input))
+    {
+        Console.WriteLine("Invalid input, try again!");
+        continue;
+    }
+    else if (input.Equals("Q", StringComparison.InvariantCultureIgnoreCase))
+        break;
+    else if (int.TryParse(input, out number))
+    {
+        Execute(number);
+    }
+    else {
+        Console.WriteLine("Invalid input, try again!");
+        continue;
+    }
+}
+
+static void Execute(int number)
+{
+    switch (number)
+    {
+        case 1: 
+            {
+                string input = "hello world from c#";
+                string output = _01_ReverseWordInSentence.ReverseWords(input);
+                Console.WriteLine(output);
+            }
+        _: return;
+    }
+}
