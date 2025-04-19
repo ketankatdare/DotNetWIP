7242cd4e0394286db37438adc5cb3cd6f9bd8855
Ketan Katdare <intech.ketan@gmail.com>
2025-04-19 14:34:55 +0530

    Adds IO Sln and Directory_File Ops project

---
+++ IO/DirAndFileOps/Program.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DirAndFileOps;
+using System.Diagnostics;
+
+//await RandomFileGenerator.Main();
+//return;
+
+var stopwatch = Stopwatch.StartNew();
+
+string folderA = @"C:\Users\ketan\Projects\Temp\folderA";
+string folderB = @"C:\Users\ketan\Projects\Temp\folderB";
+string outputFile = @"C:\Users\ketan\Projects\Temp\matches.csv";
+int countA = 0, countB = 0;
+
+try
+{
+	// Build dict for folder B
+    var fileNameToFullPathsB = new Dictionary<string, List<string>>(StringComparer.OrdinalIgnoreCase);
+    Console.WriteLine($"Loading files from {folderB}...");
+    foreach (var filePath in Directory.EnumerateFiles(folderB))
+    {
+        string fileNameWithoutExt = Path.GetFileNameWithoutExtension(filePath);
+
+        if (!fileNameToFullPathsB.TryGetValue(fileNameWithoutExt, out var list))
+        {
+            list = new List<string>();
+            fileNameToFullPathsB[fileNameWithoutExt] = list;
+        }
+
+        list.Add(filePath);
+        countB++;
+    }
+    Console.WriteLine($"Loaded {countB} filenames from {folderB}");
+
+    // Process folder A
+    Console.WriteLine($"Comparing files from {folderA}...");
+    var matches = new List<(string fileA, string fileB)>();
+    foreach (var filePathA in Directory.EnumerateFiles(folderA))
+    {
+        string fileNameWithoutExtA = Path.GetFileNameWithoutExtension(filePathA);
+        if (fileNameToFullPathsB.TryGetValue(fileNameWithoutExtA, out var matchingFilePathsB)) 
+        {
+            foreach (var matchingPathB in matchingFilePathsB)
+            {
+                matches.Add((filePathA, matchingPathB));
+                //Console.WriteLine($"Match found: {filePathA} <--> {matchingPathB}");
+            }
+        }
+        countA++;
+    }
+    Console.WriteLine($"Compared {countA} filenames from {folderA}");
+
+    // Write output
+    Console.WriteLine("Writing output file...");
+    using (var writer = new StreamWriter(outputFile))
+    {
+        writer.WriteLine($"{folderA},{folderB}");
+
+        foreach (var match in matches)
+        {
+            writer.WriteLine($"\"{match.fileA}\",\"{match.fileB}\"");
+        }
+    }
+
+    Console.WriteLine($"Matching filenames written to {outputFile}");
+}
+catch (IOException ex)
+{
+    Console.WriteLine($"IO Error: {ex.Message}");
+}
+catch (UnauthorizedAccessException ex)
+{
+    Console.WriteLine($"Access Error: {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Unexpected Error: {ex.Message}");
+}
+
+stopwatch.Stop();  // Stop timing
+Console.WriteLine($"Total time taken: {stopwatch.Elapsed}");
---
+++ IO/DirAndFileOps/RandomFileGenerator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirAndFileOps
+{
+    public static class RandomFileGenerator
+    {
+        public static async Task Main()
+        {
+            string folderA = @"C:\Users\ketan\Projects\Temp\folderA"; // 8000 files
+            string folderB = @"C:\Users\ketan\Projects\Temp\folderB"; // 800000 files
+
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1 - Create test files");
+            Console.WriteLine("2 - Delete test files");
+            Console.Write("Enter your choice: ");
+            var choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    await CreateTestFiles(folderA, 8000);
+                    await CreateTestFiles(folderB, 800000);
+                    break;
+                case "2":
+                    DeleteTestFiles(folderA);
+                    DeleteTestFiles(folderB);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+
+            Console.WriteLine("Done.");
+        }
+
+        static async Task CreateTestFiles(string folderPath, int numberOfFiles)
+        {
+            Directory.CreateDirectory(folderPath);
+
+            Console.WriteLine($"Creating {numberOfFiles} files in {folderPath}...");
+
+            for (int i = 0; i < numberOfFiles; i++)
+            {
+                string fileName = $"testfile_{i:D5}.txt"; // example: testfile_00001.txt
+                string filePath = Path.Combine(folderPath, fileName);
+
+                // Create empty file
+                using (var stream = new FileStream(filePath, FileMode.CreateNew))
+                {
+                    await stream.FlushAsync(); // optional, ensures file is really created
+                }
+
+                if (i % 1000 == 0)
+                {
+                    Console.WriteLine($"{i} files created...");
+                }
+            }
+
+            Console.WriteLine($"Finished creating {numberOfFiles} files in {folderPath}.");
+        }
+
+        static void DeleteTestFiles(string folderPath)
+        {
+            if (Directory.Exists(folderPath))
+            {
+                Console.WriteLine($"Deleting all files in {folderPath}...");
+                Directory.Delete(folderPath, true);
+                Console.WriteLine($"Deleted folder {folderPath}.");
+            }
+            else
+            {
+                Console.WriteLine($"Folder {folderPath} does not exist. Nothing to delete.");
+            }
+        }
+    }
+}
